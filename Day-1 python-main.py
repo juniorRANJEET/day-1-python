f81b4b3bf61d87c0c32212c83ed499be3b2b34bd
Ranjeet Kumar Jha <117855695+juniorRANJEET@users.noreply.github.com>
2023-02-09 23:04:49 +0530

    Add files via upload

---
+++ Day-1 python-main.py
@@ -0,0 +1,35 @@
+print("Day-1 Python prtint function")
+print("The function is declared like this:")
+print('''print('what to print')''')
+print("hello world!\nhello world!\nhello world!")
+print("hello" + " ranjeet")
+print("print('Day 1- string manipulation')")
+print('string concatining is done with "+" sign')
+print("new line can be created with backslash n")
+print("Hello "+ input("What is your name?")+ " !")
+#write a programme to print the number of user name character
+print( len( input("what is your name ?") ))
+#Varaible
+#write a programme to print the number of user name character
+name=input("what is your name? ")
+length=len(name)
+print(length)
+#Write a program to switch the valued stored in the variable "a" and "b"
+a= input("a:")
+b= input("b:")
+c=a
+a=b
+b=c
+print("a= " + a)
+print("b= "+ b)
+# create a greating for your programme
+print("welcome to the band name generator")
+# ask the user city where they grew up
+city= input("which city do you born?")
+print(city)
+# ask the user for the name of the pet
+pet= input("whta is your pet name?")
+print(pet)
+# combine the name of their city and pet to shgow them their band name
+print("your band name would be " + city +" " + pet)
+
